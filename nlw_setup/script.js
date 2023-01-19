5256eccaee85949e03752f8721710debf14cd774
Kelly Rodrigues <kellyrodrigues8519@gmail.com>
2023-01-19 17:21:53 -0300

    end of the third class

--- nlw_setup/script.js
+++ nlw_setup/script.js
@@ -2,5 +2,8 @@ const form = document.querySelector('#form-habits')
 const nlwSetup = new NLWSetup(form)
 
 const data = {
-    run: ['01-01', '01-02', '01-06']
+    run: ['01-01', '01-02', '01-06' , '01-07', '01-08']
 }
+
+nlwSetup.setData(data)
+nlwSetup.load()
